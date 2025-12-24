557ae1876971701a45bc4dfc6bfb1caa6a0deafa
PAKIWASI <ullahwasi26@gmail.com>
2025-12-24 16:57:19 +0500

    oil detection

--- lua/image_preview/init.lua
+++ lua/image_preview/init.lua
@@ -1,12 +1,19 @@
+
 local M = {}
 
+local oil
 
-local function GetFileExtension(url)
-    return url:match("^.+(%..+)$")
+-- TODO: do tmux
+local function in_tmux()
+    if os.getenv('TMUX') ~= nil then
+        return true
+    else
+        return false
+    end
 end
 
 
-local function is_kitty()
+local function in_kitty()
     if os.getenv('KITTY_PID') ~= nil then
         return true
     else
@@ -14,8 +21,25 @@ local function is_kitty()
     end
 end
 
-function M.IsImage(url)
-    local extension = GetFileExtension(url)
+local function has_oil()
+    local _has_oil, _oil = pcall(require, "oil")
+    if (_has_oil) then
+        oil = _oil
+        return true
+    else
+        return false
+    end
+end
+
+
+local function get_file_extention(url)
+    return url:match("^.+(%..+)$")
+end
+
+
+
+function M.is_image(url)
+    local extension = get_file_extention(url)
 
     if extension == '.bmp' then
         return true
@@ -30,9 +54,9 @@ function M.IsImage(url)
     return false
 end
 
-function M.PreviewImage(absolutePath)
+function M.preview_image(absolutePath)
 
-    if M.IsImage(absolutePath) then
+    if M.is_image(absolutePath) then
         -- TODO: check if correct
         vim.api.nvim_cmd(
             { cmd = "kitty +kitten icat", args = { absolutePath } },
@@ -44,20 +68,15 @@ function M.PreviewImage(absolutePath)
 end
 
 
-function M.PreviewImageOil()
-    local use, imported = pcall(require, "oil")
-    if use then
-        local entry = imported.get_cursor_entry()
+function M.preview_image_oil()
+    local entry = oil.get_cursor_entry()
 
-        if (entry and entry['type'] == 'file') then
-            local dir = imported.get_current_dir()
-            local fileName = entry['name']
-            local fullName = dir .. fileName
+    if (entry and entry['type'] == 'file') then
+        local dir = oil.get_current_dir()
+        local fileName = entry['name']
+        local fullName = dir .. fileName
 
-            M.PreviewImage(fullName)
-        end
-    else
-        return ''
+        M.preview_image(fullName)
     end
 end
 
@@ -69,3 +88,6 @@ function M.setup()
 end
 
 return M
+
+
+
